44f0aca227f6a91d8ac983e843f17ec6b6572535
MichaelWebCork <m.oconnell56@yahoo.ie>
2018-09-24 16:51:01 +0100

    started thegame

--- front_end_frameworks/hangman/frontend/src/views/Home.vue
+++ front_end_frameworks/hangman/frontend/src/views/Home.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    Homepage
+
   </div>
 </template>
 
 <script>
+import JQuery from 'jquery'
+// import words from '../words'
+var words = require('../words.json')
+const $ = JQuery
+
+$( document ).ready(function() {
+  console.log(words)
+});
+
+
 export default {
   name: 'home',
   components: {
