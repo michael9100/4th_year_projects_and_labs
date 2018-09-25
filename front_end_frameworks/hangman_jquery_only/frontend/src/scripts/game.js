9d7e95ce92aa062bd21e72cc7631f41aa9aa9c2f
Michael Web Cork <m.oconnell56@yahoo.ie>
2018-09-26 00:08:13 +0100

    button effects

--- front_end_frameworks/hangman_jquery_only/frontend/src/scripts/game.js
+++ front_end_frameworks/hangman_jquery_only/frontend/src/scripts/game.js
@@ -7,7 +7,7 @@ var alphabet = require('../alphabet.json')
 var currentGame
 
 // =============================================
-// Main Game
+// Init Game
 // =============================================
 $(document).ready(() => {
     newGame()
@@ -20,88 +20,105 @@ $(document).ready(() => {
 
 
 // =============================================
-// Functions
+// Create a new game
 // =============================================
 function newGame() {
-    var game = $('#game')
-
     // Choose a word
     currentGame = generateWord()
 
-    // Build Game
+    // Build Game Markup
     var html = ``
     html += `
-    <img src="src/imgs/hangman-0.png" alt="" id="hangImage">
-    <p id="category">Your category is: ${currentGame.category}</p>
-    <div id="input-holder">
+        <img src="src/imgs/hangman-0.png" alt="" id="hangImage">
+        <p id="category">Your category is: ${currentGame.category}</p>
+        <div id="input-holder">
     `
-
     for (var i = 0; i < currentGame.word.length; i++) {
         html += `
-            <input type="text" class="inline-input">
+            <input type="text" class="inline-input" readonly>
         `
     }
-
     html += `
-    </div>
-    <div class="button-holder">
+        </div>
+        <div class="button-holder">
     `
-
     for (var i = 0; i < alphabet.length; i++) {
         html += `
             <button class="btn alphabet" id="${alphabet[i]}">${alphabet[i]}</button>
         `
     }
-
     html += `
         </div>
     `
-
     $('#game').append(html)
 
+    // =============================================
+    // Attach Event Listers
+    // =============================================
     var inputs = $('#input-holder').children()
     var letterFound = false
     var numWrongLetters = 0
     var numCorrectLetters = 0
 
+    // Check if clicked letter is in word
     $("button.alphabet").click((btn) => {
-        console.log("FUCK")
         letterFound = false
         var letter = btn.target.innerText.toLowerCase()
         for (var i = 0; i < currentGame.word.length; i++) {
             if (letter === currentGame.word.charAt(i).toLowerCase()) {
                 inputs[i].value = letter
+                $(inputs[i]).css({
+                    'color': '#329c00',
+                    'box-shadow': '0px 0px 8px 0px #329c00',
+                    'border': 'solid 2px #fff',
+                })
                 letterFound = true
                 numCorrectLetters++
             }
         }
         $('#' + btn.target.id).attr("disabled", true)
 
+        // If the letter is NOT in the word then
+        // Increment numWrongLetters and change image
         if (letterFound == false) {
             numWrongLetters++
             $('#hangImage').attr('src', `${require('../imgs/hangman-' + numWrongLetters + '.png')}`)
         }
 
+        // Then check if the person won or not and display the modals.
         if (numCorrectLetters == currentGame.word.length) {
             $("button.alphabet").attr("disabled", true)
             resetGame()
             $('#winner').show()
         }
-
         if (numWrongLetters == 9) {
             $("button.alphabet").attr("disabled", true)
             resetGame()
             $('#looser').show()
         }
     })
+
+    // Animations
+    $("button.alphabet").hover((btn) => {
+        $('#' + btn.target.id).animate({
+            borderWidth: "5px"
+        }, 'fast')
+    }, (btn) => {
+        $('#' + btn.target.id).animate({
+            borderWidth: "0px"
+        }, 'fast')
+    })
 }
 
+
+// =============================================
+// Helper Functions
+// =============================================
 function resetGame() {
     $('#game').empty()
 }
 
 function randomInt(max) {
-    // return random number between 0 and Max
     return Math.floor(Math.random() * Math.floor(max));
 }
 
