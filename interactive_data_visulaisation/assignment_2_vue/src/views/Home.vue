b1ffeb86b43162d51ff49984b16d30633f077b3d
Michael Web Cork <m.oconnell56@yahoo.ie>
2018-12-11 01:57:10 +0000

    about to add quadtree

--- interactive_data_visulaisation/assignment_2_vue/src/views/Home.vue
+++ interactive_data_visulaisation/assignment_2_vue/src/views/Home.vue
@@ -43,6 +43,9 @@ export default {
         blue: '#1098F7',
         orange: '#FFAF0F',
       },
+      point: {
+        highlight: {},
+      }
     }
   },
   mounted() {
@@ -60,71 +63,103 @@ export default {
 
       this.map.svg = d3.select('#map')
         .append('svg')
-        .attr("width", this.map.width)
-        .attr("height", this.map.height)
-        .call(d3.zoom().on("zoom", () => {
-          this.map.svg.attr("transform", d3.event.transform)
-        }))
+          .attr("width", this.map.width)
+          .attr("height", this.map.height)
         .append('g')
-        .attr('transform', 'translate(' + this.map.margin.left + ',' + this.map.margin.top + ')')
+          .attr('transform', 'translate(' + this.map.margin.left + ',' + this.map.margin.top + ')')
 
       this.map.projection = d3.geoMercator()
         .translate([this.map.width / 2, this.map.height / 2])
         .scale(300)
         
-      var path = d3.geoPath().projection(this.map.projection)
+      let path = d3.geoPath().projection(this.map.projection)
+
+      this.point.highlight = this.map.svg
+        .append('circle')
+          .attr('r', 7)
+          .attr('style', `display: none`)
 
       this.map.svg.selectAll('.country')
         .data(this.countries)
-        .enter().append('path')
-        .attr('class', 'country')
-        .attr('fill', '#40883e')
-        .attr('stroke', '#e7e5d2')
-        .attr('d', path)
+        .enter()
+        .append('path')
+          .attr('class', 'country')
+          .attr('fill', '#40883e')
+          .attr('stroke', '#e7e5d2')
+          .attr('d', path)
 
     },
 
     drawCanvas() {
-      console.log('Drawing Sightings')
       this.map.canvas = d3.select('#map')
         .append('canvas')
-        .attr("id", 'pointsCanvas')
-        .attr("width", this.map.bbWidth)
-        .attr("height", this.map.bbHeight)
-        .attr("style", `
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-        `)
+          .attr("id", 'pointsCanvas')
+          .attr("width", this.map.bbWidth)
+          .attr("height", this.map.bbHeight)
+          .attr("style", `
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+          `)
+          .call(d3.zoom().on("zoom", () => {
+            this.map.svg.attr("transform", d3.event.transform)
+            this.onZoom()
+          }))
+      
       this.map.canvas.node().width = this.map.bbWidth
       this.map.canvas.node().height = this.map.bbHeight
       this.map.context = this.map.canvas.node().getContext('2d')
+      
+      this.map.context.fillStyle = '#3C3C3C'
+      this.map.context.strokeStyle = '#ffffff'
+
+      this.map.canvas.on('mousemove', () => {
+        let xy = [event.clientX, event.clientY]
+        this.point.highlight.attr('style', `display: block`)
+      })
 
       this.drawPoints()
     },
    
-    drawPoints() {
+    drawPoints(radius = 1) {
       this.map.context.clearRect(0, 0, this.map.bbWidth, this.map.bbHeight )
-      this.map.context.fillStyle = '#3C3C3C'
-      this.map.context.strokeStyle = '#ffffff'
 
-      // this.map.context.beginPath()
-      // this.map.context.arc(10, 10, 4, 0, Math.PI*2, false)
-      // this.map.context.fill()
-      // this.map.context.stroke()
 
       let d
-      var coords 
+      var coords
       for (let i = 0; i < this.sightings.length; i++) {
         d = this.sightings[i]
         coords = this.map.projection([d.longitude, d.latitude])
         this.map.context.beginPath()
-        this.map.context.arc(coords[0], coords[1], 3, 0, Math.PI*2)
+        this.map.context.arc(coords[0], coords[1], radius, 0, Math.PI*2)
         this.map.context.fill()
-        this.map.context.stroke()
+        // this.map.context.stroke()
       }
+    },
+
+    onZoom() {
+      var transform = d3.event.transform;
+
+      // Check how far the zoom is and make an appropriate radius
+      let zoomLevel = 1
+      // abstracted this val to a var as it was calculated 3 times
+      let k = transform.k / 7
+      if (k > 1 ) {
+        if (1 / (k) <= 0.03) {
+          zoomLevel = 0.03
+        } else {
+          zoomLevel = 1 / k
+        }
+      }
+
+      this.map.context.save()
+      this.map.context.clearRect(0, 0, this.map.bbWidth, this.map.bbHeight)
+      this.map.context.translate(transform.x, transform.y)
+      this.map.context.scale(transform.k, transform.k)
+      this.drawPoints(zoomLevel)
+      this.map.context.restore()
     }
   }
 
